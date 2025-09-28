89574348108487bdbeafb08ba1e6a91106949052
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-09-28 21:37:37 +0200

    finished

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class Word {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        String returnString = word();
+        System.out.println(returnString);
     }
 
+    public static String word() {
+        return "Hello World";
+    }
 }
