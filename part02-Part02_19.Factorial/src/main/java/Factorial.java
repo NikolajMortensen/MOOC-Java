190fffb37f6ef262eaecce8942a80886bb5b07c3
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-09-28 10:04:19 +0200

    Finished exercise

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,18 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number: ");
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        int sum = 1;
+        int n = 0;
+
+        while (n < number) {
+            n++;
+            sum = n * sum;
+
+
+        }
+        System.out.println(sum);
     }
 }
