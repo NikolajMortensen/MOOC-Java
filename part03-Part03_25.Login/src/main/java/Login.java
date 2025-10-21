4d5aa07972982426c1bc88f00e21d8340534d50a
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-10-21 12:53:59 +0200

    finished exercises

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -5,9 +5,9 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String username1 = "Alex";
+        String username1 = "alex";
         String password1 = "sunshine";
-        String username2 = "Emma";
+        String username2 = "emma";
         String password2 = "haskell";
 
         System.out.println("Enter username: ");
@@ -16,11 +16,11 @@ public class Login {
         System.out.println("Enter password: ");
         String password = scanner.nextLine();
 
-        if (!(username.equals(username1 ||
-                              username) && password.equals(password1 || password2))) {
-            System.out.println("Try again!");
+        if ((username.equals(username1) && password.equals(password1)) ||
+            (username.equals(username2) && password.equals(password2))) {
+            System.out.println("You have successfully logged in!");
         } else {
-            System.out.println("You succesfully logged in!");
+            System.out.println("Incorrect username or password!");
         }
 
 
--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,21 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String text = scanner.nextLine();
+            String[] pieces = text.split(" ");
+
+            if (text.isEmpty()) {
+                break;
+            }
+
+            for (String word : pieces) {
+                if (word.contains("av")) {
+                    System.out.println(word);
+                }
+            }
+
+        }
 
     }
 }
