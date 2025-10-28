d1386c81b41d5396cc9e6b39e3eb653087e9f783
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-10-28 13:00:33 +0100

    finished exercises

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -10,7 +10,36 @@ public class MainProgram {
 
         // In order for the tests to work, the objects must be created in the
         // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
+        // sum, secondly the object that tracks the sum of even numbers,
         // and lastly the one that tracks the sum of odd numbers!
+
+        Statistics sumOfNumbStat = new Statistics();
+        Statistics sumOfEvenStat = new Statistics();
+        Statistics sumOfOddStat = new Statistics();
+
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == -1) {
+                break;
+            } 
+
+            sumOfNumbStat.addNumber(number);
+
+            if (number % 2 != 0 ) {
+                sumOfOddStat.addNumber(number);
+            } else {
+                sumOfEvenStat.addNumber(number);
+            }
+
+        }
+        
+        System.out.println("Sum: " + sumOfNumbStat.sum());
+        System.out.println("Sum of even numbers: " + sumOfEvenStat.sum());
+        System.out.println("Sum of odd numbers: " + sumOfOddStat.sum());
+        System.out.println("Average: " + sumOfNumbStat.average());
+
     }
 }
